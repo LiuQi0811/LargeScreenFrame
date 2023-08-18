01bfa49084bd9ed603cae9d8f930a6e6837aad94
LIUQI <1139642444@qq.com>
2023-08-18 17:10:57 +0800

    LargeScreenFrame 初始化

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/**
+ * 初始化 app
+ */
+async function initApp(){
+    // 创建app 对象实例化
+   const app = createApp(App)
+   // 挂载到页面
+   app.mount('#app',true)
+}
+
+// 初始化 app
+initApp()
