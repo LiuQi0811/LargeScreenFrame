5fe31a49b82e02c731a5aee2a8471ba8f06c7e0e
LIUQI <1139642444@qq.com>
2023-08-26 17:03:57 +0800

    KuiErAppProvider index.vue 新增插槽占位 <slot/>

--- src/components/KuiErAppProvider/index.vue
+++ src/components/KuiErAppProvider/index.vue
@@ -1,10 +1,16 @@
 <template>
+  <strong style="color: white;font-size: 30px">登录页面</strong>
+  <!--加载条组件-->
   <n-loading-bar-provider>
+    <!--对话框组件-->
     <n-dialog-provider>
       <dialog-content/>
+      <!--通知组件-->
       <n-notification-provider>
+      <!--信息组件-->
        <n-message-provider>
-         FG
+         <!--插槽就是子组件中的提供给父组件使用的一个占位符，用<slot></slot> 表示，父组件可以在这个占位符中填充任何模板代码，如 HTML、组件等，填充的内容会替换子组件的<slot></slot>标签。-->
+         <slot/>
        </n-message-provider>
       </n-notification-provider>
     </n-dialog-provider>
